ece1b5812a0852fe6341f19ba4f06f6306cfaa80
Anup K <anupkulkarni945@gmail.com>
2025-02-25 09:27:30 +0530

    added gaussian sum

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ def add (a,b):
 def remainder (a,b):
     return a%b 
 
+def sum_of_n (n):
+    sum = 0
+    for i in range(n+1):
+        sum +=i
+    return sum
+
 def mod(a):
     if a>=0:
         return a
@@ -13,3 +19,5 @@ def mod(a):
 print(mod(add(-7,3)))
 
 print(remainder(1973,7))
+
+print(sum_of_n(100))
